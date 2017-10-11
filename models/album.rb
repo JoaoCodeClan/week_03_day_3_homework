d709619513267247f026cc617776dbb2a99a5ad1
Joao <johnynecas@hotmail.com>
2017-10-11 22:39:28 +0100

    added delete, delete_all and update to album class

--- models/album.rb
+++ models/album.rb
@@ -4,7 +4,8 @@ require_relative("artist.rb")
 
 class Album
 
-  attr_reader(:id, :title, :genre, :artist_id)
+  attr_reader(:id)
+  attr_accessor(:title, :genre, :artist_id)
 
   def initialize(album)
     @id = album['id'].to_i if album['id']
@@ -37,7 +38,24 @@ class Album
     return list_of_artists.map {|artist| Artist.new(artist)}
   end
 
+  def delete()
+    sql = "DELETE FROM album WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql,values)
+  end
 
+  def self.delete_all()
+    sql = "DELETE FROM album"
+    values = []
+    SqlRunner.run(sql,values)
+    return "All albums cleared"
+  end
+
+  def update()
+    sql = " UPDATE album SET(title,genre,artist_id)=($1,$2,$3) WHERE id = $4"
+    values = [@title,@genre,@artist_id, @id]
+    SqlRunner.run(sql,values)
+  end
 
 
 end
